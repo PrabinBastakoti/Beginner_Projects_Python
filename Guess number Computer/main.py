7b0cb79d11da9e40fc7066622d9cb4b5c5e5217a
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2022-08-07 06:47:48 +0545

    Edited loop in Guess Number Computer

--- Guess number Computer/main.py
+++ Guess number Computer/main.py
@@ -5,16 +5,16 @@ def computer_guess(x):
     low = 1
     high = x 
     feedback = ''
-    while feedback != 'c' and 'C':
+    while feedback != 'c' or feedback ==  'C':
         if low != high :
             guess = random.randint(low, high)
         else :
             guess = low # could be high as well since low = high
         feedback= input(f"Is {guess} too High ( H ), too Low ( L ), or correct ( C ) ?? ")
-        if feedback == 'h' and 'H':
+        if feedback == 'h' or feedback == 'H':
             high = guess - 1
 
-        if feedback == 'l' and 'L':
+        elif feedback == 'l' or feedback== 'L':
             low = guess + 1
         
     print(f"Yay! The computer guessed your number, {guess}, correctly!")
